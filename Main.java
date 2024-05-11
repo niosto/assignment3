57f539f3b8f02d37796ad5facb70e5797d5cd532
niosto <nicolasospinat@gmail.com>
2024-05-10 22:12:28 -0500

    Commit con metodo de impresion

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
 public class Main {
     public static void main(String arg []){
+        Imprimir();
+    }
+    
+    public static void Imprimir(){
+        System.out.println("Hecho por Nicolás Ospina Torres");
     }
 }
